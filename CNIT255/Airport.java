89a0af95939fe73d9639e29b189e18e3b90a8793
Alex Gurekovich <alexgurekovich@gmail.com>
2021-11-16 22:40:18 -0500

    Creation of Airport class

---
+++ CNIT255/Airport.java
@@ -0,0 +1,61 @@
+package CNIT255;
+
+public class Airport {
+
+    // Attributes of the class
+
+
+    private String Country;
+    private String City;
+    private String Flights;
+    private String Airlines;
+
+    public Airport(String Country, String City, String Flights, String Airlines) {
+        
+        // Setters being declared for the attribute
+
+        setCountry(Country);
+        setCity(City);
+        setFlights(Flights);
+        setAirlines(Airlines);
+    }
+
+    
+    // Create set methods
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public void setFlights(String Flights) {
+        this.Flights = Flights;
+    }
+
+    public void setAirlines(String Airlines) {
+        this.Airlines = Airlines;
+    }
+
+
+    // Create getter methods for the class
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getFlights() {
+        return Flights;
+    }
+
+    public String getAirlines() {
+        return Airlines;
+    }
+
+}
