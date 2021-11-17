0e97b7d0f08253b4fd05d0e342874e9f24a3f415
Alex Gurekovich <alexgurekovich@gmail.com>
2021-11-16 23:37:44 -0500

    Creating getters and setters

---
+++ Airlines.java
@@ -0,0 +1,25 @@
+package CNIT255;
+
+public class Airlines {
+
+    // Additional attributes needed for this class
+
+    private String Country;
+
+    public Airlines() {
+
+    }
+
+    // Create setter methods for the class
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    // Create getter methods for the class
+
+    public String getCountry() {
+        return Country;
+    }
+
+}
